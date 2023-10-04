d5f88965703e6ae80e2d78c0678751fb9b79f691
Godwin <godwinmail6@gmail.com>
2023-10-04 16:23:43 +0200

    class created

---
+++ Sim.java
@@ -0,0 +1,26 @@
+
+public class Sim {
+
+    public int number;
+    public String credit;
+
+
+    class CallList {
+        public String[] friends;
+
+
+
+    }
+
+    public Sim(int number, String credit, String list){
+        this.number = number;
+        this.credit = credit;
+
+    }
+
+    public void callSummary()
+    {
+        /*System.out.println("Phone number" + this.number + " " + this.credit + " " + );*/
+    }
+
+}
