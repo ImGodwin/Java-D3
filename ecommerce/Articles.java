f7b0cc7a37f7e9acc9ce5da6cfa3869ec57e5e1f
Godwin <godwinmail6@gmail.com>
2023-10-05 07:21:58 +0200

    created new classes for ecommerce package.
    ps: there is an error message from the rectangle package, after I deleted a file that is not supposed to have any effect on it. I will be fixing this problem later, as it does not affect the current package in which i am working on.

---
+++ ecommerce/Articles.java
@@ -0,0 +1,15 @@
+package ecommerce;
+
+public class Articles {
+
+    private int id;
+    public String description;
+    private int price;
+    public int productsAvail;
+
+
+    public  Articles(int id, String description, int price, int productsAvail){
+        this.id = id;
+        this.description = description
+    }
+}
